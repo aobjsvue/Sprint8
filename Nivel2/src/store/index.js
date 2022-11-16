bda506b2723fbe86789797c8e93fcea78b5ab995
aobjsvue <andres.orta@hotmail.com>
2022-11-16 13:07:59 +0100

    nivel2uploaded

---
+++ Nivel2/src/store/index.js
@@ -0,0 +1,126 @@
+import router from '@/router';
+import { createStore } from 'vuex'
+
+export default createStore({
+  state: {
+    starshipsList: [],
+    page: 1,
+    starship: [],
+    loginModal: false,
+    registerModal: false,
+    usersDB: [],
+    logged: false,
+    loggedUser: "",
+    pilots: [],
+  },
+  getters: {
+  },
+  mutations: {
+    setStarshipsList(state, starships) {
+      state.starshipsList = starships;
+    },
+    setMoreStarships(state, moreStarships) {
+      if (state.starshipsList.length < moreStarships.count) {
+        state.starshipsList = state.starshipsList.concat(moreStarships.results);
+      }
+    },
+    setPage(state) {
+      if (state.page < 5) {
+        state.page++;
+      }
+    },
+    setStarship(state, starshipInfo) {
+      state.starship = starshipInfo;
+    },
+    toggleLoginModal(state) {
+      state.loginModal = !state.loginModal;
+    },
+    toggleRegisterModal(state) {
+      state.registerModal = !state.registerModal;
+    },
+    createAccount(state, userData) {
+      const checkEmail = state.usersDB.map((user) => user.email.toLowerCase());
+      if (!checkEmail.includes(userData.email.toLowerCase())) {
+        state.usersDB.push(userData);
+        state.registerModal = false;
+        localStorage.setItem('user', JSON.stringify(state.usersDB));
+        console.log("Register completed. You can log in now!");
+      } else {
+        console.log("A user already exists with that e-mail address.");
+      }
+    },
+    logIn(state, userData) {
+      if (state.usersDB.some((user) => userData.email === user.email)) {
+        if (state.usersDB.some((user) => userData.password === user.password)) {
+          state.loggedUser = userData.email;
+          state.logged = true;
+          state.loginModal = false;
+          console.log("Log in successful!");
+        } else {
+          state.logged = false;
+          console.log("Wrong email address or password.");
+        }
+      } else {
+        state.logged = false;
+        state.loginModal = false;
+        state.registerModal = true;
+        console.log("There is no registered user with that e-mail address.");
+      }
+    },
+    logOut(state) {
+      state.logged = false;
+      state.loggedUser = '';
+      router.push({name: 'home'});
+      console.log("Log out successful!")
+    },
+    checkDataBase(state) {
+      const dataBase = JSON.parse(localStorage.getItem('user'));
+      if ( dataBase === null) {
+        state.usersDB = [];
+      } else {
+        state.usersDB = dataBase;
+      }
+    },
+    setPilots(state, pilotsData) {
+      pilotsData.forEach((url) => {
+        fetch(url)
+          .then((response) => response.json())
+          .then((data) => {
+            state.pilots.push(data);
+          });
+      });
+    },
+    emptyPilots(state) {
+      state.pilots = [];
+    },
+    toggleModals(state) {
+      state.loginModal = !state.loginModal;
+      state.registerModal = !state.registerModal;
+    },
+  },
+  actions: {
+    async fetchStarships ({ commit }) {
+      const response = await fetch("https://swapi.dev/api/starships/?page=" + this.state.page);
+      const data = await response.json();
+      commit('setStarshipsList', data.results);
+    },
+    async fetchMoreStarships ({ commit }) {
+      commit('setPage');
+      if (this.state.page > 4) {
+        return
+      } else {
+        const response = await fetch("https://swapi.dev/api/starships/?page=" + this.state.page);
+        const data = await response.json();
+        commit('setMoreStarships', data);
+      }
+    },
+    async fetchStarshipInfo ({ commit }, id) {
+      const response = await fetch(`https://swapi.dev/api/starships/${id}/`);
+      const data = await response.json();
+      commit('setStarship', data);
+      commit('setPilots', data.pilots);
+    }
+  },
+  modules: {
+  }
+})
---
+++ Nivel2/src/views/HomeView.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="welcome">
+    <div class="title">
+      <p>THE LATEST</p>
+    </div>
+    <div class="content">
+      <h2>A long time ago in a galaxy far, far away...</h2>
+      <h4>Starships of all kinds were piloted across the universe...</h4>
+      <p>Finally, we managed to get a model of each of them and gathered together in detail in our database. Our starship's dock is now more complete than ever!</p>
+      <p>Come on, take a look!</p>
+    </div>
+    <router-link to="/starships">
+      <button type="button">GET ACCESS TO THE DOCK</button>
+    </router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeView'
+}
+</script>
+
+<style>
+  .welcome {
+    text-align: left;
+    max-width: 90%;
+    margin: 1.5em auto;
+  }
+  .title p {
+    color: #ddd;
+    border-top: 2px solid #3b3b3b;
+    border-bottom: 2px solid #3b3b3b;
+    font-weight: 600;
+    letter-spacing: 1px;
+    padding: 0.5em 0 0.5em 1em;
+    background-color: rgba(21, 21, 21, 0.4);
+  }
+  .content {
+    background-color: rgba(21, 21, 21, 0.8);
+    padding: 1em;
+    border-radius: 0.1em;
+  }
+  .content h2, h4 {
+    color: #ddd;
+    margin-top: 0;
+    text-transform: uppercase;
+  }
+  .content p {
+    color: #aaa;
+    margin-bottom: 0;
+  }
+</style>
